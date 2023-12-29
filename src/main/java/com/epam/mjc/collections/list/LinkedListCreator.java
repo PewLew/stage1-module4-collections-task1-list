673fde6b23a70a7e4edcd4549a6198df9ec6f752
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2023-12-29 13:33:26 +0600

    y

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (Integer integer : sourceList) {
+            if (integer % 2 == 0) {
+                list.addLast(integer);
+            } else
+                list.addFirst(integer);
+        }
+        return list;
     }
 }
